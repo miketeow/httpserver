6e30580c523d3c35e61edd1fbebb1c87f3a57c93
miketeow <27071053+miketeow@users.noreply.github.com>
2025-12-20 22:37:52 +0800

    json chapter done

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"slices"
+	"strings"
 	"sync/atomic"
 )
 
@@ -10,34 +14,97 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
+type chirpResponse struct {
+	CleanedBody string `json:"cleaned_body"`
+}
+
+type errorResponse struct {
+	Error string `json:"error"`
+}
+
 func (cfg *apiConfig) middlewareMetricInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w,r)
+		next.ServeHTTP(w, r)
 	})
 }
 
-func (cfg *apiConfig) showHits(w http.ResponseWriter, r *http.Request){
-	w.Header().Add("Content-Type","text/plain; charset=utf-8")
+func (cfg *apiConfig) showHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w,"Hits: %d", cfg.fileserverHits.Load())
+	fmt.Fprintf(w, `<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.fileserverHits.Load())
 }
 
-func (cfg *apiConfig) resetHits(w http.ResponseWriter, r *http.Request){
-	w.Header().Add("Content-Type","text/plain; charset=utf-8")
+func (cfg *apiConfig) resetHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	cfg.fileserverHits.Swap(0)
 }
 
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJSON(w,code,errorResponse{Error: msg})
+}
 
-func main(){
+func respondWithJSON(w http.ResponseWriter, code int, payload any) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
+func getCleanedBody(msg string) string {
+	profaneWords := []string{"kerfuffle","sharbert","fornax"}
+	words := strings.Split(msg, " ")
+	cleanedMsg := strings.Split(msg, " ")
+	for index, word := range words {
+		if slices.Contains(profaneWords,strings.ToLower(word)){
+			cleanedMsg[index] = "****"
+		}
+	}
+	result := strings.Join(cleanedMsg, " ")
+	return result
+}
+
+func handleJson(w http.ResponseWriter, r *http.Request) {
+	type parameter struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameter{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 500, "Something went wrong")
+		return
+	} else if len(params.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+		return
+	} else {
+		cleaned := getCleanedBody(params.Body)
+		respondWithJSON(w, 200, chirpResponse{
+			CleanedBody: cleaned,
+		})
+	}
+}
+
+func main() {
 	apiCfg := &apiConfig{
 		fileserverHits: atomic.Int32{},
 	}
 
 	mux := http.NewServeMux()
 	server := &http.Server{
-		Addr: ":8080",
+		Addr:    ":8080",
 		Handler: mux,
 	}
 
@@ -45,17 +112,18 @@ func main(){
 
 	wrappedHandler := apiCfg.middlewareMetricInc(fsHandler)
 
-	mux.Handle("/app/",http.StripPrefix("/app/",wrappedHandler))
+	mux.Handle("/app/", http.StripPrefix("/app/", wrappedHandler))
 
-	mux.HandleFunc("GET /healthz",func(w http.ResponseWriter, req *http.Request){
-		w.Header().Add("Content-Type","text/plain; charset=utf-8")
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		data := []byte("OK")
 		w.Write(data)
 	})
 
-	mux.HandleFunc("GET /metrics", apiCfg.showHits)
-	mux.HandleFunc("POST /reset", apiCfg.resetHits)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.showHits)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetHits)
+	mux.HandleFunc("POST /api/validate_chirp", handleJson)
 
 	server.ListenAndServe()
 }
